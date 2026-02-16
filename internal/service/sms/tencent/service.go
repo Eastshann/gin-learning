65cbfbd064977a4111ba7940221da6bbdf29f71b
gufan-laptop <gufan0721@163.com>
2026-02-17 01:28:13 +0800

    短信验证业务

---
+++ internal/service/sms/tencent/service.go
@@ -0,0 +1,57 @@
+package tencent
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ecodeclub/ekit"
+	"github.com/ecodeclub/ekit/slice"
+	sms "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms/v20210111"
+)
+
+type Service struct {
+	appId    *string
+	signName *string
+	client   *sms.Client
+}
+
+func NewService(client *sms.Client, appId string, signName string) *Service {
+	return &Service{
+		appId:    ekit.ToPtr[string](appId),
+		signName: ekit.ToPtr[string](signName),
+		client:   client,
+	}
+}
+
+func (s Service) Send(ctx context.Context, tplId string, args []string, numbers ...string) error {
+	req := sms.NewSendSmsRequest()
+	req.SmsSdkAppId = s.appId
+	req.SignName = s.signName
+	req.TemplateId = ekit.ToPtr[string](tplId)
+	req.TemplateParamSet = s.toPtrSlice(args)
+	req.PhoneNumberSet = s.toPtrSlice(numbers)
+	resp, err := s.client.SendSms(req)
+	// 处理异常
+	if err != nil {
+		return err
+	}
+	for _, statusPtr := range resp.Response.SendStatusSet {
+		if statusPtr == nil {
+			// 不可能进来这里
+			continue
+		}
+		status := *statusPtr
+		if status.Code == nil || *(status.Code) != "Ok" {
+			// 发送失败
+			return fmt.Errorf("发送短信失败 code: %s, msg: %s", *status.Code, *status.Message)
+		}
+	}
+	return nil
+}
+
+func (s *Service) toPtrSlice(data []string) []*string {
+	return slice.Map[string, *string](data,
+		func(idx int, src string) *string {
+			return &src
+		})
+}
